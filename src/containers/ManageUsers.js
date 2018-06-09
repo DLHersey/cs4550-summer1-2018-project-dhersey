9d7666c46d2c1191c002ce78df84c8c80a7df3ac
David Hersey <hersey.da@husky.neu.edu>
2018-06-09 15:23:37 -0700

    created userManager

---
+++ src/containers/ManageUsers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+export default class ManageUsers
+    extends React.Component {
+
+
+    render() {
+        return(
+            <div>
+                Manage Users
+            </div>
+        )
+    }
+
+}
