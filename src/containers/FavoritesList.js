44f451ef9852fc0014d6b2a6b707dfc3fd087011
David Hersey <hersey.da@husky.neu.edu>
2018-06-25 18:02:12 -0700

    fixing rendering

--- src/containers/FavoritesList.js
+++ src/containers/FavoritesList.js
@@ -13,45 +13,56 @@ export default class FavoritesList
         }
         this.removeFavorite = this.removeFavorite.bind(this);
         this.findAllFavoritesForUser = this.findAllFavoritesForUser.bind(this);
+        this.setUserId=this.setUserId.bind(this);
+    }
+
+    setUserId(userId) {
+        this.setState({userId: userId});
     }
 
     componentDidMount() {
-//        this.findAllFavoritesForUser(auth.getId());
-        this.findAllFavoritesForUser(this.props.userId);
+        let userId = parseInt(this.props.userId, 10);
+        this.setUserId(userId);
+        this.findAllFavoritesForUser(userId);
+    }
+
+    componentWillReceiveProps(newProps){
+        this.setUserId(newProps.userId);
+        this.findAllFavoritesForUser(newProps.userId);
     }
 
     findAllFavoritesForUser(userId) {
         this.favoriteService
             .findAllFavoritesForUser(userId)
-            .then((favorites) => {
-                console.log(favorites);
-                this.state.favorites = favorites;
-            })
+            .then((response) => {
+                this.setState({favorites: response});
+            });
     }
 
     removeFavorite(fid) {
         this.favoriteService
             .deleteFavorite(fid)
-            .then(() => this.findAllFavoritesForUser(auth.getId()));
+            .then(() => this.findAllFavoritesForUser(this.props.userId));
     }
 
 
     renderFavoritesList() {
         let favorites = this.state.favorites.map((favorite) => {
             return (
-            <li key={favorite.id}>
-                <h3>{favorite.label}</h3>
-                <button className="btn btn-danger"
-                        type="button"
-                onClick={this.removeFavorite(favorite.id)}>Remove</button>
-            </li>)
+                <li key={favorite.id}>
+                    <h3>{favorite.label}</h3>
+                    <button className="btn btn-danger"
+                            type="button"
+                    onClick={this.removeFavorite(favorite.id)}>Remove</button>
+                </li>
+            )
         })
     }
 
     render() {
         return(
             <div>
-                <h1>List goes here</h1>
+                <h3>Favorite Recipes: </h3>
                 <ul>
                     {this.renderFavoritesList()}
                 </ul>
